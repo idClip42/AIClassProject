1fb488f7b404eb5e4aa04902adb8e1375c45f6e3
Alexander Earley <axe6801@rit.edu>
2017-03-07 11:48:54 -0500

    Finished documenting code. Some notes: - Each white sphere is a path node, representing its position and radius - Upon starting the game, they all move down to terrain level and disappear - Assuming the debug lines checkbox in the pathfinder object is checked, purple lines are drawn in between all connecting nodes, and green lines are drawn representing the path (but with extra branches - each node draws a green line to the node set as its previous node)

--- Assets/Scripts/AStarNode.cs
+++ Assets/Scripts/AStarNode.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class AStarNode : MonoBehaviour 
 {
+	/// <summary>
+	/// The nodes adjacent to this one
+	/// </summary>
 	public AStarNode[] adjacentNodes;
 
+
 	float startToCurrent = float.MaxValue;
-	public float StartToCurrent { get { return startToCurrent; } set { startToCurrent = value; } }
+	/// <summary>
+	/// The cost from the start node to this one, along a path
+	/// </summary>
+	/// <value>The cost from the start node to this one</value>
+	public float StartToCurrent { 
+		get { return startToCurrent; }
+		set { startToCurrent = value; }
+	}
+
 
 	float currentToEnd = float.MaxValue;
-	public float CurrentToEnd { get { return currentToEnd; } set { currentToEnd = value; } }
+	/// <summary>
+	/// The cost from this node to the end node, via Manhattan Distance
+	/// </summary>
+	/// <value>The cost from this node to the end node</value>
+	public float CurrentToEnd { 
+		get { return currentToEnd; }
+		set { currentToEnd = value; }
+	}
 
-	public float TotalCost 
+
+	/// <summary>
+	/// The sum of startToCurrent and currentToEnd
+	/// (unless one or both is unchanged from MaxValue)
+	/// </summary>
+	/// <value>The total cost.</value>
+	public float TotalCost
 	{ 
 		get 
 		{
@@ -23,11 +48,23 @@ public class AStarNode : MonoBehaviour
 	}
 
 	AStarNode previousNode = null;
-	public AStarNode PreviousNode { get { return previousNode; } set { previousNode = value; } }
+	/// <summary>
+	/// The previous node to this one in the path
+	/// </summary>
+	/// <value>The previous node.</value>
+	public AStarNode PreviousNode { 
+		get { return previousNode; } 
+		set { previousNode = value; } 
+	}
 
+
+	/// <summary>
+	/// Gets the Manhattan Distance between this and another node
+	/// </summary>
+	/// <returns>The Manhattan Distance between the two nodes.</returns>
+	/// <param name="node">The other node</param>
 	public float GetDistBetween(AStarNode node)
 	{
-		// Uses manhattan distance for speed
 		float x = Mathf.Abs(node.transform.position.x - transform.position.x);
 		float y = Mathf.Abs(node.transform.position.y - transform.position.y);
 		float z = Mathf.Abs(node.transform.position.z - transform.position.z);
@@ -38,18 +75,22 @@ public class AStarNode : MonoBehaviour
 
 
 	bool debugLinesOn = false;
+	/// <summary>
+	/// Whether debug lines will show node connections and paths
+	/// </summary>
+	/// <value><c>true</c> if debug lines on; otherwise, <c>false</c>.</value>
 	public bool DebugLinesOn { set { debugLinesOn = value; } }
 
 	void Update()
 	{
-		if(debugLinesOn == true)
+		if(debugLinesOn == true)				// If the debug lines are on
 		{
 			for(int n = 0; n < adjacentNodes.Length; ++n)
-				Debug.DrawLine(
+				Debug.DrawLine(					// Purple lines are drawn between all nodes
 					transform.position, 
 					adjacentNodes[n].transform.position, 
 					Color.magenta);
-			if(previousNode != null)
+			if(previousNode != null)			// Green lines are drawn to each node's previous node
 				Debug.DrawLine(
 					transform.position + Vector3.one, 
 					previousNode.transform.position + Vector3.one + Vector3.up * 2, 
@@ -64,6 +105,7 @@ public class AStarNode : MonoBehaviour
 	{
 
 		// Ensures that all node connections go both ways
+		// by adding node to adjacent lists of adjacent nodes
 
 		for(int n = 0; n < adjacentNodes.Length; ++n)
 		{
@@ -96,7 +138,9 @@ public class AStarNode : MonoBehaviour
 
 	}
 
-
+	/// <summary>
+	/// Resets the distance values and previous node
+	/// </summary>
 	public void Reset()
 	{
 		startToCurrent = float.MaxValue;
--- Assets/Scripts/AStarPathfinder.cs
+++ Assets/Scripts/AStarPathfinder.cs
@@ -4,117 +4,128 @@ using UnityEngine;
 
 public class AStarPathfinder : MonoBehaviour 
 {
-	public bool debugLines;
+	public static AStarPathfinder pathfinder;	// The public pathfinder, easily accessible
 
-	List<AStarNode> open;
-	List<AStarNode> closed;
+	public bool debugLines;						// Whether the scene will visualize the nodes and paths
 
-	AStarNode[] nodes;
+	AStarNode[] nodes;							// The list of all nodes
+	List<AStarNode> open;						// The list of open nodes (for A*)
+	List<AStarNode> closed;						// The list of closed nodes (for A*)
 
-	public static AStarPathfinder pathfinder;
-
-
-	/*
-	public AStarNode startNodeManual;
-	public AStarNode endNodeManual;
-	public bool findPath;
-*/
-
-
-	int remainingNodeCount;
-
+	int remainingNodeCount;						// How many nodes have not been put in the closed list
+												// Used to ensure that the algorithm does not loop infinitely
+												// if there are problems in the code
 
 
 
 	void Start()
 	{
-		pathfinder = this;
+		pathfinder = this;						// Initializes this pathfinder as public static object
 
+												// Gets all the nodes in the scene
 		nodes = GetComponentsInChildren<AStarNode>();
 		for(int n = 0; n < nodes.Length; ++n)
 		{
-			nodes[n].DebugLinesOn = debugLines;
+			nodes[n].DebugLinesOn = debugLines;	// Turns on debug lines for each node (or not)
 			nodes[n].GetComponent<MeshRenderer>().enabled = false;
+												// Turns off the mesh renderer so world is not filled with white spheres
 		}
 	}
 
 
-
-	/*
-	void Update()
-	{
-		if(findPath == true)
-		{
-			findPath = false;
-			GetPath(startNodeManual, endNodeManual);
-		}
-	}
-*/
-
-
-
+	/// <summary>
+	/// Gets a path from one node to another
+	/// </summary>
+	/// <returns>The path.</returns>
+	/// <param name="start">The starting node</param>
+	/// <param name="end">The ending node</param>
 	public Stack<Transform> GetPath(AStarNode start, AStarNode end)
 	{
-		open = new List<AStarNode>();
-		closed = new List<AStarNode>();
-		remainingNodeCount = nodes.Length;
-		foreach(AStarNode node in nodes)
-			node.Reset();
+		Reset();								// Resets all the nodes and lists
 
-		start.StartToCurrent = 0;
+		start.StartToCurrent = 0;				// Sets the start node's cost so far to 0
+									
+												// Starts the A* process
+												// If it returns true, a path has been found
+												// and is then returns
 		if(RecursiveAStarStep(start, end) == true)
 		{
-			Debug.Log("Got a path");
+			Debug.Log("Found a path");
 			return ReturnPath(start, end);
 		}
 		else
 		{
-			Debug.Log("Didn't got a path");
+			Debug.Log("Didn't find a path");	// Otherwise, nothing is returned
 			return null;
 		}
 	}
 
+	/// <summary>
+	/// A recursive method that is a step in the A* algorithm
+	/// </summary>
+	/// <returns><c>true</c>, if a path is found, <c>false</c> otherwise.</returns>
+	/// <param name="currentNode">The current path node to be examined</param>
+	/// <param name="endNode">The end goal</param>
 	bool RecursiveAStarStep(AStarNode currentNode, AStarNode endNode)
 	{
-		InfiniteLoopStopper();
+		InfiniteLoopStopper();					// Prevents any recursive screw-ups, just in case
 
-		closed.Add(currentNode);
+		closed.Add(currentNode);				// Adds the current node to the closed list
+												// It will not be examined again
+												// Valid adjacent nodes are added to the open list
 		AddAdjacentsToOpen(currentNode, endNode);
-		AStarNode nextNode = GetShortestNode();
-		if(nextNode == null)
-			return false;	// No path found
-		if(nextNode == endNode)
-			return true; // Path found
-		open.Remove(nextNode);
+		AStarNode nextNode = GetShortestNode();	// The next currentNode is the node with the
+												// lowest cost to the end in the open list
+		if(nextNode == endNode)					// If the next node is the end, we've found our path
+			return true;
+		if(nextNode == null)					// If there is no next node, no path has been found
+			return false;
+		open.Remove(nextNode);					// The next node is removed from the open list
+												// (to be added to the closed list)
 
 		return RecursiveAStarStep(nextNode, endNode);
+												// This method is called again for the next node
 	}
 
+	/// <summary>
+	/// Adds adjacent nodes of the input node to the open list
+	/// Also gives them cost info
+	/// </summary>
+	/// <param name="node">The current node to check the adjacents of</param>
+	/// <param name="end">The end node</param>
 	void AddAdjacentsToOpen(AStarNode node, AStarNode end)
 	{
-		AStarNode[] nodes = node.adjacentNodes;
+		AStarNode[] nodes = node.adjacentNodes;	// Gets the adjacent nodes
 		for(int n = 0; n < nodes.Length; ++n)
 		{
-			if(!closed.Contains(nodes[n]))
+			if(!closed.Contains(nodes[n]))		// If the adjacent nodes aren't in the closed list
 			{
 				float costSoFar = node.StartToCurrent;
 				float hereToThere = nodes[n].GetDistBetween(node);
 				float thereToEnd = nodes[n].GetDistBetween(end);
+												// We find their costs relative to the current node
 			
 				if(!open.Contains(nodes[n]) || nodes[n].TotalCost > (hereToThere + costSoFar + thereToEnd)) 
 				{
 					nodes[n].StartToCurrent = hereToThere + costSoFar;
 					nodes[n].CurrentToEnd = thereToEnd;
 					nodes[n].PreviousNode = node;
+												// If the total cost from the current node is less than
+												// what the node currently has,
+												// it is given the new cost values
 
 					if(!open.Contains(nodes[n])) 
-						open.Add(nodes[n]);
+						open.Add(nodes[n]);		// If it is not in the open list, it is added
 				}
 
 			}
 		}
 	}
 
+	/// <summary>
+	/// Gets the node in the open list with the lowest cost
+	/// </summary>
+	/// <returns>The node with the lowest cost, or null if list is empty</returns>
 	AStarNode GetShortestNode()
 	{
 		if(open.Count == 0) return null;
@@ -125,7 +136,10 @@ public class AStarPathfinder : MonoBehaviour
 		return current;
 	}
 
-
+	/// <summary>
+	/// Ensures that the recursive method doesn't keep going forever
+	/// </summary>
+	/// <returns><c>true</c>, loop must be stopped, <c>false</c> otherwise.</returns>
 	bool InfiniteLoopStopper()
 	{
 		--remainingNodeCount;
@@ -137,7 +151,12 @@ public class AStarPathfinder : MonoBehaviour
 		return false;
 	}
 
-
+	/// <summary>
+	/// Returns an ordered stack of transforms that holds the path of nodes
+	/// </summary>
+	/// <returns>The path stack</returns>
+	/// <param name="start">Start node</param>
+	/// <param name="end">End node</param>
 	Stack<Transform> ReturnPath(AStarNode start, AStarNode end)
 	{
 		Stack<Transform> nodeStack = new Stack<Transform>();
@@ -150,6 +169,19 @@ public class AStarPathfinder : MonoBehaviour
 		}
 
 		return nodeStack;
+	}
+
+
+	/// <summary>
+	/// Resets the nodes and the lists for a new path
+	/// </summary>
+	void Reset()
+	{
+		open = new List<AStarNode>();
+		closed = new List<AStarNode>();
+		remainingNodeCount = nodes.Length;
+		foreach(AStarNode node in nodes)
+			node.Reset();
 	}
 
 
