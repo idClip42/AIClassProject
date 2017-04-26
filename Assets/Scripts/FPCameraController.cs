a74671376b3f7b6d9bc6e33ac31d68e9d32091a2
alecbiela <alec.bielanos@gmail.com>
2017-04-26 02:52:28 -0400

    Added Project 4 Base
    Added some things to start the base of Project 4:
    -Disabled old (not used) objects, put them in a parent gameobject called
    "Old Objects"
    -Changed terrain back to default layer (for raycasting enable)
    -Moved isometric camera to be top-down camera
    
    +Added unit prefab, a capsule with InfluenceUnit script on it
    +The InfluenceUnit script has a method called InitUnit which sets color
    and strength of the unit based on the number passed in (int)
    +FPCameraController.cs is attached to the top-down camera and handles
    all input/creation of units
    +WASD movement of camera (can adjust sensitivity of movement in
    inspector), Q and E to zoom out and in, respectively.
    +R and F have been stubbed out to restart the simulation and generate
    the influence map, respectively.
    +ZXCV will spawn units of strength 1234 (respectively) at the mouse
    position.

---
+++ Assets/Scripts/FPCameraController.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPCameraController : MonoBehaviour {
+
+    //set the sensitivity of camera movement in the inspector
+    public float CAMERA_MOVE_SPEED;
+
+    //prefab that we will use for units (capsule) - color & strength are set at time of instantiation
+    public GameObject UNIT_PREFAB;
+
+
+    //the last gameobject that we instantiated (so we can access its color and strength)
+    private GameObject _lastAdded;
+
+    // Use this for initialization
+    void Start () {
+        if (!UNIT_PREFAB) Debug.LogError("No Unit Prefab has been assigned in the inspector!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+        //GETKEY for camera movement
+
+
+        //W Pressed, move +Z
+        if(Input.GetKey(KeyCode.W))
+        {
+            this.transform.Translate(new Vector3(0, CAMERA_MOVE_SPEED * Time.deltaTime, 0));
+        }
+        //S Pressed, move -Z
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(new Vector3(0, -CAMERA_MOVE_SPEED * Time.deltaTime, 0));
+        }
+        //D Pressed, move +X
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(new Vector3(CAMERA_MOVE_SPEED * Time.deltaTime, 0, 0));
+        }
+        //A Pressed, move -X
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(new Vector3(-CAMERA_MOVE_SPEED * Time.deltaTime, 0, 0));
+        }
+        //E Pressed, move +Y
+        if (Input.GetKey(KeyCode.E))
+        {
+            this.transform.Translate(new Vector3(0, 0, CAMERA_MOVE_SPEED * Time.deltaTime));
+        }
+        //Q Pressed, move -Y
+        if (Input.GetKey(KeyCode.Q))
+        {
+            this.transform.Translate(new Vector3(0, 0, -CAMERA_MOVE_SPEED * Time.deltaTime));
+        }
+
+
+        //KEYDOWN for instantiating new units & MISC
+
+
+        //Z Pressed, instantiate 1-strength unit (White)
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            _lastAdded = GameObject.Instantiate(UNIT_PREFAB);
+            _lastAdded.GetComponent<InfluenceUnit>().InitUnit(1);
+        }
+        //X Pressed, instantiate 2-strength unit (Blue)
+        else if (Input.GetKeyDown(KeyCode.X))
+        {
+            _lastAdded = GameObject.Instantiate(UNIT_PREFAB);
+            _lastAdded.GetComponent<InfluenceUnit>().InitUnit(2);
+        }
+        //C Pressed, instantiate 3-strength unit (Yellow)
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            _lastAdded = GameObject.Instantiate(UNIT_PREFAB);
+            _lastAdded.GetComponent<InfluenceUnit>().InitUnit(3);
+        }
+        //V Pressed, instantiate 4-strength unit (Black)
+        else if (Input.GetKeyDown(KeyCode.V))
+        {
+            _lastAdded = GameObject.Instantiate(UNIT_PREFAB);
+            _lastAdded.GetComponent<InfluenceUnit>().InitUnit(4);
+        }
+
+
+
+        //F Pressed, re-generate influence map
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            //stub
+        }
+
+        //R Pressed, restart simulation
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            //stub
+        }
+    }
+
+
+}//end FPCameraController.cs
---
+++ Assets/Scripts/InfluenceUnit.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfluenceUnit : MonoBehaviour {
+
+    //the strength of this unit, with accessor
+    private int _strength;
+    public int Strength { get; }
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //sets up this unit to be the strength passed in
+    public void InitUnit(int str)
+    {
+        //identify the strength and set color
+        switch (str)
+        {
+            case 1:     //white
+                //prefab is white by default
+                break;
+            case 2:     //blue
+                this.GetComponent<Renderer>().material.color = Color.blue;
+                break;
+            case 3:     //yellow
+                this.GetComponent<Renderer>().material.color = Color.yellow;
+                break;
+            case 4:     //black
+                this.GetComponent<Renderer>().material.color = Color.black;
+                break;
+            default:    //any other number that's passed in
+                Debug.Log("Strength of unit is out of bounds, setting to 1...");
+                str = 1;
+                break;
+        }
+
+        //set Strength (will be changed to 1 if out of bounds) and position
+        _strength = str;
+        this.transform.position = setUpPosition();
+    }
+
+
+    //helper method to set up position of unit
+    private Vector3 setUpPosition()
+    {
+        //see if the mouse is over the terrain
+        RaycastHit hit;
+        Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        //if we hit the terrain
+        if(Physics.Raycast(r, out hit, 1000))
+        {
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = hit.distance;
+
+            //get the mouse position in world space and adjust Y to terrain height
+            Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            worldMousePos.y = GameObject.FindGameObjectWithTag("Terrain")
+                .GetComponent<Terrain>().SampleHeight(worldMousePos)
+                + 1.1f;
+
+            //return mouse pos w/ terrain height adjustment
+            return worldMousePos;
+        }
+
+        //if we get here, we didn't hit it, so return 0
+        return Vector3.zero;
+    }
+}
